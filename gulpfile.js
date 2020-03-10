fa83f0a0a67aa225a6e69bda76fe40302b5a3806
Alejandro D. Bakker <abakker@si-mad.com>
2020-03-10 15:33:45 +0100

    Alpha Demo: Gulp Improved [BrowserSyn for Auto Reload].

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
 /* Gulp test file */
 
 // Tools
-let gulp     = require('gulp');
-let concat   = require('gulp-concat');
-let cleanCss = require('gulp-clean-css');
-let jshint   = require('gulp-jshint');
-let minify   = require('gulp-minify');
-let uglify   = require('gulp-uglify-es').default;
+let gulp        = require('gulp');
+let concat      = require('gulp-concat');
+let cleanCss    = require('gulp-clean-css');
+let jshint      = require('gulp-jshint');
+let minify      = require('gulp-minify');
+let uglify      = require('gulp-uglify-es').default;
+let browserSync = require('browser-sync').create();
+let reload      = browserSync.reload;
 
 /* FACTSET + ini */
 
 // Task Style
-let strFSCssSrc  = ['css/styleFSCurrencyConverter.css'];
+let
+    strFSCssSrc  = ['css/styleFSCurrencyConverter.css'];
 let strFSCssDest = 'css/';
 gulp.task('tskFSStyle', function () {
     return gulp.src(strFSCssSrc)
                .pipe(concat('styleFSCurrencyConverter.min.css'))
                .pipe(cleanCss({level: {1: {specialComments: 0}}}))
                .pipe(gulp.dest(strFSCssDest))
+               .pipe(reload({stream: true}));
 });
 
 // Task Js
@@ -29,6 +33,20 @@ gulp.task('tskFSJs', function () {
                .pipe(jshint())
                .pipe(uglify())
                .pipe(gulp.dest(strFSJsDest))
+               .pipe(reload({stream: true}));
+});
+
+// Browser refresh
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(['js/jFactSetRatesInput.js', 'js/jFactSetCurrencyConverter.js', 'css/styleFSCurrencyConverter.css'],
+        gulp.series(['tskFSStyle', 'tskFSJs', browserSync.reload]));
+
 });
 
 // FACTSET Task
@@ -41,5 +59,8 @@ gulp.task('tsFSkWatch', function () {
     gulp.watch(['js/jFactSetRatesInput.js', 'js/jFactSetCurrencyConverter.js', 'css/styleFSCurrencyConverter.css'],
         gulp.series(['tskFSStyle', 'tskFSJs']));
 });
+
+// Default
+gulp.task('default', gulp.parallel(['browser-sync', 'tsFSkWatch']));
 
 /* FACTSET - fin */
